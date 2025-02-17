069026eee4197efa0aae8d2522a2b761b0a08235
Murali Krishna <murali.gedela@hexaviewtech.com>
2025-02-17 18:44:29 +0530

    add redirect url

---
+++ src/supabase.js
@@ -0,0 +1,9 @@
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
+  {
+    auth: {
+      redirectTo: 'https://auto-tube-web-v2.vercel.app'
+    }
+  }
+) 
